ec84b33132e909e9d4f04606dca427789755c7d3
joaorio01 <jonnyvictor.rio@gmail.com>
2026-03-12 20:47:26 -0300

    Add files via upload

---
+++ Java/java.js
@@ -0,0 +1,13 @@
+window.alert("ESSE É UM ALERTA!!");
+console.log("ESSE É OUTRO ALERTA");
+
+let dado = prompt("Entre com um dado: ");
+
+console.log(dado);
+
+let i = 0;
+
+while(i<10) {
+    window.alert(i);
+    i++;
+}

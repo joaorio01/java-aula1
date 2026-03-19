906a9448aac0855f06855aaa3656408c9f8f4eb1
joaorio01 <jonnyvictor.rio@gmail.com>
2026-03-19 20:42:25 -0300

    Add files via upload

---
+++ canvas JAVA/arquivo.js
@@ -0,0 +1,91 @@
+let canvas = document.getElementById("canvas")
+let ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'gray';
+ctx.strokeStyle = 'gray';
+ctx.fillRect(0, 280, 700, 300);
+ctx.strokeRect(0, 280, 700, 300);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = 'brown';
+ctx.strokeStyle = 'brown';
+ctx.fillRect(150, 180, 100, 100);
+ctx.strokeRect(150, 180, 100, 100);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'blue';
+ctx.fillRect(160, 210, 25, 25);
+ctx.strokeRect(160, 210, 25, 25);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'blue';
+ctx.fillRect(215, 210, 25, 25);
+ctx.strokeRect(215, 210, 25, 25);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.strokeStyle = 'black';
+ctx.fillRect(193, 240, 15, 40);
+ctx.strokeRect(193, 240, 15, 40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = 'brown';
+ctx.strokeStyle = 'brown';
+ctx.fillRect(350, 300, 15, 40);
+ctx.strokeRect(350, 300, 15, 40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = 'brown';
+ctx.strokeStyle = 'brown';
+ctx.fillRect(40, 240, 15, 40);
+ctx.strokeRect(40, 240, 15, 40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'green';
+ctx.strokeStyle = 'green';
+ctx.arc(48,215,28,1.5*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'green';
+ctx.strokeStyle = 'green';
+ctx.arc(358, 275,28,1.5*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'yellow';
+ctx.arc(320, 100,40,1.5*Math.PI,5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'red';
+ctx.strokeStyle = 'red';
+ctx.moveTo(250, 180);
+ctx.lineTo(200,130);
+ctx.lineTo(150,180);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
